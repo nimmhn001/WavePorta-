7babdf6d88b046a39feaa7213866898c0d56cf95
Nimisha Mohan <85029169+nimmhn001@users.noreply.github.com>
2022-07-03 22:07:47 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,7 +14,3 @@ module.exports = {
 
 
 
-
-// Deploying contracts with account:  0xF74d046Ca8e47CDd508f2C803872a63f394bd535
-// Account balance:  10000000000000000
-// WavePortal address:  0xFcb4dF34A53361d0737221d531Aa924b23D87Cf9
